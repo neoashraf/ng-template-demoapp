0d1171759f5cd2dd37be705a649bacde8332b0ab
neo <ashrafuddin160@gmail.com>
2019-12-24 03:46:17 +0600

    created project and added a child app

---
+++ src/app/child-app/child-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-app',
+  templateUrl: './child-app.component.html',
+  styleUrls: ['./child-app.component.scss']
+})
+export class ChildAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
